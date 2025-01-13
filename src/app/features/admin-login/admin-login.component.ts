bfd7c5cab655f05b289603183099ea0e4bb4c1a4
shakti <mahabli@yahoo.com>
2025-01-14 00:21:05 +0530

    added velzon theme

--- src/app/features/admin-login/admin-login.component.ts
+++ src/app/features/admin-login/admin-login.component.ts
@@ -8,55 +8,40 @@ import { ButtonModule } from 'primeng/button';
 import { Ripple } from 'primeng/ripple';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { NgxSpinnerModule } from "ngx-spinner";
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-admin-login',
-  imports: [ReactiveFormsModule, NgIf, ToastModule, Toast, ButtonModule, NgxSpinnerModule, FormsModule, CommonModule],
+  imports: [ReactiveFormsModule, ToastModule, ButtonModule, NgxSpinnerModule, FormsModule, CommonModule],
   templateUrl: './admin-login.component.html',
   styleUrl: './admin-login.component.scss'
 })
 export class AdminLoginComponent implements OnInit {
   loginForm!: FormGroup;
+  showPassword: boolean = false;
+  passwordFieldType: 'password' | 'text' = 'password';
 
-  constructor(private _fb: FormBuilder, private messageService: MessageService, private _spinner: NgxSpinnerService,) { }
+  constructor(private _fb: FormBuilder, private messageService: MessageService,
+    private _spinner: NgxSpinnerService, private router: Router) { }
 
   ngOnInit(): void {
-    this.initilizeLoginForm();
-  }
-  showSpinner() {
-    this._spinner.show();
-    // Hide spinner after 3 seconds
-    setTimeout(() => {
-      this._spinner.hide();
-    }, 3000);
-  }
-  initilizeLoginForm() {
     this.loginForm = this._fb.group({
-      userName: ['', [Validators.required]],
-      password: ['', [Validators.required]]
-    })
+      email: ['', Validators.required],
+      password: ['', Validators.required]
+    });
   }
-  get userName() {
-    return this.loginForm.controls['userName']
-  }
-  get password() {
-    return this.loginForm.controls['password']
-  }
-  submit() {
-    this._spinner.show()
-    // this.messageService.add({ severity: 'info', summary: 'Info', detail: 'Message Content', life: 3000 });
 
-    if (!this.loginForm.valid) {
-      this.loginForm.markAllAsTouched();
+  signIn(): void {
+    const { email, password } = this.loginForm.value;
+    if (this.loginForm.controls['email'].value == "any101t5@gmail.com" && this.loginForm.controls['password'].value == "12345") {
+      console.log('Login successful');
+      this.router.navigate(['']);
+    } else {
+      console.error('Login error');
+      alert('Invalid credentials! \n \n Please Enter These Credential \n Username: any101t5@gmail.com \n Password: 12345')
     }
-    if (this.loginForm.valid) {
-      if (this.loginForm.controls['userName'].value == "sunilsingh" || this.loginForm.controls['password'].value == "Sunil@123") {
-        alert('success');
-      }
-      if (this.loginForm.controls['userName'].value !== "sunilsingh" || this.loginForm.controls['password'].value !== "Sunil@123") {
-        alert('invalid user name or password');
-      }
-    }
-    console.log(this.loginForm.value);
+  }
 
+  togglePasswordVisibility(): void {
+    this.passwordFieldType = this.passwordFieldType === 'password' ? 'text' : 'password';
   }
 }
